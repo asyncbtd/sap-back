846291bd1ad2e209a40fec13a8b67b231c0e59ec
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-10-20 22:44:42 +0300

    init project

---
+++ src/test/java/io/github/asyncbtd/sap/SapApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.asyncbtd.sap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
